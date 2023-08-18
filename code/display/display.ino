d2ab96132ced680a2e2da26c64405593ad7260a6
AntarktisZ11 <marcus.borne@outlook.com>
2023-08-18 15:58:57 +0200

    Remove need for Pico W

--- code/display/display.ino
+++ code/display/display.ino
@@ -2,10 +2,8 @@
 #include <Fonts/FreeSerifBold9pt7b.h>
 #include <Thread.h>
 #include <StaticThreadController.h>
-#include <WiFi.h>
 #include "timer.h"
 #include "pico/stdlib.h"
-#include "pico/cyw43_arch.h"  
 #include "logo.h"
 
 // Color definitions used for setting text and background color
@@ -50,19 +48,13 @@ Timer timer;
 long buzzer_timestamp = 0;
 int start_grace_period_us = 1500000; // 1.5 sec start grace period
 
-int port = 80;
-char ssid[] = "Bamses Øhlhäfvarstoppur";
-char pass[] = "test1234";
-int status = WL_IDLE_STATUS;
-WiFiServer server(port);
 
 String httpRequest;
 
 Thread timerThread = Thread();
 Thread displayThread = Thread();
-Thread wifiThread = Thread();
 Thread buzzerThread = Thread();
-StaticThreadController<4> controller (&timerThread, &displayThread, &wifiThread, &buzzerThread);
+StaticThreadController<4> controller (&timerThread, &displayThread, &buzzerThread);
 
 void setup(void) {
   Serial.begin(115200);
@@ -96,141 +88,22 @@ void setup(void) {
 
   timerThread.onRun(updateTimer);
   displayThread.onRun(updateDisplay);
-  wifiThread.onRun(updateWifi);
   buzzerThread.onRun(updateBuzzer);
 
   timerThread.setInterval(1); // update every 1ms
   displayThread.setInterval(1); // update every 1ms
-  wifiThread.setInterval(100); // update every 100ms
   buzzerThread.setInterval(100); // update every 100ms
   
-  // check for the WiFi module:
-  if (WiFi.status() == WL_NO_MODULE) {
-    Serial.println("Communication with WiFi module failed!");
-    while (true); // Don't continue
-  }
-
-  String fv = WiFi.firmwareVersion();
-
-  if (fv < WIFI_FIRMWARE_LATEST_VERSION) {
-    Serial.println("Please upgrade the firmware");
-  }
-
-  uint8_t wifiStatus = WiFi.beginAP(ssid, pass);
-
-  if (wifiStatus != WL_CONNECTED) {
-    Serial.println("Creating access point failed");
-    
-    while (true); // Don't continue
-  }
 
   // Startup screen
   matrix.fillScreen(BLACK);
   matrix.drawRGBBitmap(0, 0, logo, 64, 32);
   matrix.show();
-
-  // Wait for wifi module to load
-  delay(5000); // TODO add constant for this
-
-  server.begin();
-
-  printWiFiStatus();
 }
 
-void printWiFiStatus() {
-
-  // print the SSID of the network you're attached to:
-  Serial.print("SSID: ");
-  Serial.println(WiFi.SSID());
-
-  // print your WiFi shield's IP address:
-  IPAddress ip = WiFi.localIP();
-  Serial.print("IP Address: ");
-  Serial.println(ip);
-
-  // print where to go in a browser:
-  Serial.print("To see this page in action, open a browser to http://");
-  Serial.println(ip);
-}
 
 void loop(void) {
   controller.run();
-}
-
-void updateWifi() {
-  // Listen for incoming clients
-  WiFiClient client = server.available();
-
-  if (client) {
-
-    Serial.println("new client");
-    // An http request ends with a blank line
-    bool currentLineIsBlank = true;
-    while (client.connected()) {
-      if (client.available()) {
-        char c = client.read();
-        
-        httpRequest += c;
-
-        // if you've gotten to the end of the line (received a newline
-        // character) and the line is blank, the http request has ended,
-        // so you can send a reply
-        if (c == '\n' && currentLineIsBlank) {
-
-          Serial.println(httpRequest); //print client http request
-
-          // send a standard http response header
-          client.println("HTTP/1.1 200 OK");
-          client.println("Content-Type: text/html");
-          client.println("Connection: close");  // the connection will be closed after completion of the response
-          //client.println("Refresh: 5");  // refresh the page automatically every 5 sec
-          client.println();
-          client.println("<!DOCTYPE HTML>");
-          client.println("<html>");
-          client.println("<title>Bamses Øhlhäfvarstoppur Admin panel</title>");
-          client.println("<h1 style=\"color: #5e9ca0;\">Tidtagarkontrollpanel</h1>");
-          client.println("<a href=/?start>START TIMER</a>");
-          client.println("<a href=/?stop>STOP TIMER</a>");
-          client.println("<a href=/?reset>RESET TIMER</a>");
-          client.println("<br>");
-          // TODO add timer value
-          client.println("</html>");
-
-          break;
-        }
-
-        if (c == '\n') {
-          // you're starting a new line
-          currentLineIsBlank = true;
-        } else if (c != '\r') {
-          // you've gotten a character on the current line
-          currentLineIsBlank = false;
-        }
-      }
-    }
-
-    // give the web browser time to receive the data
-    delay(1);
-
-    // close the connection:
-
-    client.stop();
-    Serial.println("client disconnected");
-
-    if (httpRequest.indexOf("?start") > 0 ) {
-      timer.start();
-    }
-
-    if (httpRequest.indexOf("?stop") > 0 ) {
-      timer.stop();
-    }
-
-    if (httpRequest.indexOf("?reset") > 0 ) {
-      timer.reset();
-    }
-
-    httpRequest = "";
-  }
 }
 
 void updateBuzzer() {
